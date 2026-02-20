bffbf3e3480e2807942724cffa6dc91df771dde1
EhaLotafe <overcomeeha@gmail.com>
2026-02-20 12:15:15 +0200

    Fix Server/Client boundary in services page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import HeroSection from '@/components/HeroSection'
 import AboutSection from '@/components/AboutSection'
 import ServicesSection from '@/components/ServicesSection'
--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import ServicesSection from '../../components/ServicesSection'
 
 export default function ServicesPage() {
